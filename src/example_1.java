7a259890bea218689a4dc5eaf33bdcf24073da12
Yusuke Minei <e175703@ie.u-ryukyu.ac.jp>
2017-11-28 00:14:38 +0900

    implement new variable

--- src/example_1.java
+++ src/example_1.java
@@ -1,8 +1,8 @@
 public class example_1 {
     public static void main(String[] args){
         try {
-            String str = null;
-            System.out.println(str.length());
+            String str = "3．14";
+            double value = Double.parseDouble(str);
         } catch (NullPointerException e) {
             System.out.println("ぬるぽ");
             System.out.println(e.getMessage());
